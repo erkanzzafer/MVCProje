c4a8dd8474698e3790926ba525bb6ac0fd563baa
erkanzzafer <zzafererkann@gmail.com>
2022-10-07 19:58:11 +0300

    key ve string length attributleri eklendi

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,19 @@ namespace EntityLayer.Concrete
 {
     public class About
     {
+        [Key]
         public int AboutID { get; set; }
 
+        [StringLength (500) ]
         public string AboutDetails1 { get; set; }
+
+        [StringLength(1000)]
         public string AboutDetails2 { get; set; }
+
+        [StringLength(100)]
         public string AboutImage1 { get; set; }
+
+        [StringLength(100)]
         public string AboutImage2 { get; set; }
     }
 }
